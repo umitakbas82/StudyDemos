a003fbc930a409bad0d215f9c01fef0be23da034
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-23 23:34:46 +0300

    working with northwind db, adding nuget packages

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //ADO.NET
+           //ENTITYFRAMEWORK= ORM(OBJECT RETATIONSHIP MAPPING)
+            
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
