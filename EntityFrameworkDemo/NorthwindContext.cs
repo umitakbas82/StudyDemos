4f2e5787c09fec695e738a36712db679b02cc387
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-01-08 00:41:08 +0300

    Connect database and LINQ query added

--- EntityFrameworkDemo/NorthwindContext.cs
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -9,5 +9,22 @@ namespace EntityFrameworkDemo
 {
     public class NorthwindContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
     }
+
+
+
+
+
+    
+
+
+
 }
--- EntityFrameworkDemo/Program.cs
+++ EntityFrameworkDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace EntityFrameworkDemo
 {
@@ -6,12 +7,37 @@ namespace EntityFrameworkDemo
     {
         static void Main(string[] args)
         {
-           //ADO.NET
-           //ENTITYFRAMEWORK= ORM(OBJECT RETATIONSHIP MAPPING)
-            
-            
-            
-            Console.WriteLine("Hello World!");
+            //ADO.NET
+            //ENTITYFRAMEWORK= ORM(OBJECT RETATIONSHIP MAPPING)
+            //GetAll();
+            GetProductByCategory(1);
+
         }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+        }
+
+        private static void GetProductByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+        }
+
+
+
+
     }
 }
